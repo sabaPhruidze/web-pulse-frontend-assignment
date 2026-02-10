93040b5fc4d8780ed75c99bcb9d2b7b9894ae5cb
Saba <sabaphruidze01@gmail.com>
2026-02-10 11:46:55 +0400

    This is an assigment , given my the company pulse so it is old version and in order to follow the warning added future attribute for solving it *React Router Future Flag Warning: Relative route resolution within Splat routes is changing in v7. You can use the 7_relativeSplatPath future flag to opt-in early. For more information, see https://reactrouter.com/v6/upgrading/future#v7_relativesplatpath.*

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <Routes>
         <Route path="/" element={<Dashboard />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
