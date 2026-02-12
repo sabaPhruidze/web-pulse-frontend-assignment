5372bd09bec9a13016de1fc0c819e9decea64fab
Saba <sabaphruidze01@gmail.com>
2026-02-12 12:29:10 +0400

    visual part for top gainers done, now will use map and add 3 top gainer

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -21,22 +21,41 @@ const Dashboard = () => {
         ) : isError ? (
           <div>error</div>
         ) : (
-          <SectionCard title="Portfolio Summary">
-            <div className="my-3">
-              <p className="text-pulse-soft font-semibold text-sm">
-                Total Value
-              </p>
-              <strong className="text-pulse-text text-2xl">{totalValue}</strong>
-            </div>
-            <div>
-              <p className="text-pulse-soft font-semibold text-sm">
-                Total Change
-              </p>
-              <strong className="text-pulse-success/80 ">
-                ${totalChange} (+{totalChangePercent}%)
-              </strong>
-            </div>
-          </SectionCard>
+          <>
+            <SectionCard title="Portfolio Summary">
+              <div className="my-3">
+                <p className="text-pulse-soft font-semibold text-sm">
+                  Total Value
+                </p>
+                <strong className="text-pulse-text text-2xl">
+                  {totalValue}
+                </strong>
+              </div>
+              <div>
+                <p className="text-pulse-soft font-semibold text-sm">
+                  Total Change
+                </p>
+                <strong className="text-pulse-success/80 ">
+                  ${totalChange} (+{totalChangePercent}%)
+                </strong>
+              </div>
+            </SectionCard>
+            <SectionCard title="Top Gainers">
+              <div className="">
+                <p className="text-pulse-text font-bold">
+                  NVDA{" "}
+                  <span className="font-semibold text-pulse-soft text-[14px]">
+                    NVIDIA Corporation
+                  </span>
+                </p>
+                <p className="text-xs text-pulse-soft font-semibold">$485.20</p>
+                <p className="text-pulse-success/80 text-sm font-bold">
+                  +3.45%
+                </p>
+                <hr className="border-pulse-border mt-5" />
+              </div>
+            </SectionCard>
+          </>
         )}
       </div>
     </div>
