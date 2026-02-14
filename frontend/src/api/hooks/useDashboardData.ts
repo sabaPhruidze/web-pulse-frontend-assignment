9723dc1ef59cd250056078f54ee8e81452e29dc0
Saba <sabaphruidze01@gmail.com>
2026-02-14 11:46:14 +0400

    perf(useDashboardData): added getNews in tanstack query

--- frontend/src/api/hooks/useDashboardData.ts
+++ frontend/src/api/hooks/useDashboardData.ts
@@ -1,23 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAssets } from "../endpoints/assets";
 import { getPortfolio } from "../endpoints/portfolio";
+import { getNews } from "../endpoints/news";
 import type { Assets } from "../../types/assets";
 import type { Portfolio } from "../../types/portfolio";
-
+import type { News } from "../../types/news";
 type DashboardData = {
   portfolio: Portfolio;
   assets: Assets;
+  news: News;
 };
 
 const useDashboardData = () => {
   return useQuery<DashboardData>({
     queryKey: ["dashboard"],
     queryFn: async () => {
-      const [portfolio, assets] = await Promise.all([
+      const [portfolio, assets, news] = await Promise.all([
         getPortfolio(),
         getAssets(),
+        getNews(),
       ]);
-      return { portfolio, assets };
+      return { portfolio, assets, news };
     },
   });
 };
