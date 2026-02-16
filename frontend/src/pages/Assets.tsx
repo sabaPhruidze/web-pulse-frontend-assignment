7e53bda5f7fda6269507ecee83559f9e7a97c13a
Saba <sabaphruidze01@gmail.com>
2026-02-16 17:45:03 +0400

    update(Assets): Added searchbar and buttons which will filter according to which one is clicked but I will move them into ui as components

--- frontend/src/pages/Assets.tsx
+++ frontend/src/pages/Assets.tsx
@@ -7,7 +7,28 @@ function Assets() {
       title="Assets"
       subtitle="Unified view of stocks and crypto currencies"
     >
-      <div>fewf</div>
+      {/* Tabs */}
+      <div className="mt-4 flex gap-2">
+        <button className="border border-pulse-border px-3 py-2 rounded-lg bg-pulse-surface text-pulse-text text-sm font-semibold">
+          All
+        </button>
+        <button className="border border-pulse-border px-3 py-2 rounded-lg bg-pulse-surface text-pulse-text text-sm font-semibold">
+          {" "}
+          Stocks Only
+        </button>
+        <button className="border border-pulse-border px-3 py-2 rounded-lg bg-pulse-surface text-pulse-text text-sm font-semibold">
+          Crypto Only
+        </button>
+      </div>
+      <div>
+        <div className="mt-3">
+          <input
+            type="text"
+            placeholder="Search by symbol or name..."
+            className="w-full px-3 py-2 rounded-lg bg-pulse-surface border-2 border-pulse-border text-pulse-text placeholder:text-pulse-soft focus:outline-none focus:ring-2 focus:ring-pulse-primary transition-shadow duration-300 ease-in-out"
+          />
+        </div>
+      </div>
     </PageLayout>
   );
 }
