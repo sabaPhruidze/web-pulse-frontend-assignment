cd37b74723ff338ad7d7080365e5404d4975c712
Saba <sabaphruidze01@gmail.com>
2026-02-16 17:12:07 +0400

    update(App): added assets route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
 const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Assets = lazy(() => import("./pages/Assets"));
 function App() {
   return (
     <BrowserRouter
@@ -18,6 +14,7 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/assets" element={<Assets />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
