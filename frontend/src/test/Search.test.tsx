5b0edaf5a37ebf24744d4cbaf95fcaa06963ed60
Saba <sabaphruidze01@gmail.com>
2026-02-24 11:49:53 +0400

    test(Search): third test was to test when closed the dropdownMenu it will not display the menu again

--- frontend/src/test/Search.test.tsx
+++ frontend/src/test/Search.test.tsx
@@ -3,9 +3,10 @@ import { describe, test, expect } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 import Search from "../components/header/Search";
 import userEvent from "@testing-library/user-event";
-
-describe("Header Search", () => {
-  test("renders search input", () => {
+import { fireEvent } from "@testing-library/react";
+describe("Header's Search", () => {
+  //codes in test's are sometimes repeated because ech test is independent from another
+  test("renders search input , exists search or not", () => {
     render(
       <MemoryRouter>
         <Search />
@@ -15,7 +16,7 @@ describe("Header Search", () => {
     const input = screen.getByRole("searchbox");
     expect(input).toBeInTheDocument();
   });
-  test("Open dropdown menu when user clicks the search input", async () => {
+  test("Open dropdown menu when user clicks the search input and checks if there will be pages menu", async () => {
     const user = userEvent.setup();
     render(
       <MemoryRouter>
@@ -28,5 +29,22 @@ describe("Header Search", () => {
       screen.getByRole("button", { name: /dashboard/i }), //case insentive
     ).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /assets/i })).toBeInTheDocument();
+  });
+  // sometimes fireEvent.mouseDown works better and is controlled in easier way on forward click so I will use it
+  //something like TouchableWithoutFeedback in order to dismiss keyboard in react native expo
+  test("closes dropDown menu when the user clicks outside of it", async () => {
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <Search />
+      </MemoryRouter>,
+    );
+    const input = screen.getByRole("searchbox");
+    await user.click(input);
+    // Dropdown is open and then Assets will be displayed
+    expect(screen.getByRole("button", { name: /assets/i })).toBeInTheDocument();
+    //when clicked outside of it , dropdown menu musst be closed and assets will not be displayed
+    fireEvent.mouseDown(document.body);
+    expect(screen.queryByRole("button", { name: /assets/i })).toBeNull();
   });
 });
