f168d76542b2da8a69d0f9a6f0e764a083c79762
Saba <sabaphruidze01@gmail.com>
2026-03-04 10:46:47 +0400

    get back to normal

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: [
-      "https://web-pulse-frontend-assignment-5qfgnispc-sabaphruidzes-projects.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false,
-  }),
-);
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 
 // API Routes
